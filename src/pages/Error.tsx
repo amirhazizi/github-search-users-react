a61b7c228eeced831a091cfab12cc488241d6792
amir <amirazizihosein@gmail.com>
2023-04-20 00:25:15 +0430

    setup routers

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react"
+import styled from "styled-components"
+
+import { Link } from "react-router-dom"
 const Error = () => {
-  return <h2>error page</h2>;
-};
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page yout tried cannot be found</h3>
+        <Link to='/' className='btn'>
+          Back to Home
+        </Link>
+      </div>
+    </Wrapper>
+  )
+}
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
@@ -17,5 +28,5 @@ const Wrapper = styled.section`
     color: var(--clr-grey-3);
     margin-bottom: 1.5rem;
   }
-`;
-export default Error;
+`
+export default Error
