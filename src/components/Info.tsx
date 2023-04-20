33ecb995fe3159bd7781d94a82a7c1e11961f865
amir <amirazizihosein@gmail.com>
2023-04-20 21:26:01 +0430

    info Component

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React from "react"
+import { useGlobalGithubContext } from "../context/context"
+import styled from "styled-components"
+import { GoRepo, GoGist } from "react-icons/go"
+import { FiUsers, FiUserPlus } from "react-icons/fi"
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
-};
+  const { githubUser } = useGlobalGithubContext() as {
+    githubUser: {
+      public_repos: number
+      followers: number
+      following: number
+      public_gists: number
+    }
+  }
+  const { public_repos, followers, following, public_gists } = githubUser
+  const items = [
+    {
+      id: 1,
+      icon: <GoRepo className='icon' />,
+      label: "repos",
+      value: public_repos,
+      color: "pink",
+    },
+    {
+      id: 2,
+      icon: <FiUsers className='icon' />,
+      label: "followers",
+      value: followers,
+      color: "green",
+    },
+    {
+      id: 3,
+      icon: <FiUserPlus className='icon' />,
+      label: "following",
+      value: following,
+      color: "purple",
+    },
+    {
+      id: 4,
+      icon: <GoGist className='icon' />,
+      label: "gists",
+      value: public_gists,
+      color: "yellow",
+    },
+  ]
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {items.map(({ id, icon, label, value, color }) => {
+          return (
+            <article key={id} className='item'>
+              <span className={color}>{icon}</span>
+              <div>
+                <h3>{value}</h3>
+                <p>{label}</p>
+              </div>
+            </article>
+          )
+        })}
+      </Wrapper>
+    </section>
+  )
+}
 
 const Wrapper = styled.section`
   display: grid;
@@ -58,6 +113,6 @@ const Wrapper = styled.section`
       color: #f0b429;
     }
   }
-`;
+`
 
-export default UserInfo;
+export default UserInfo
