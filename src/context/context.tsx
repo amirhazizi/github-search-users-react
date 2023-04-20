3f8ee13ba12d2dfb93cdd86944c34ef765ca268e
amir <amirazizihosein@gmail.com>
2023-04-20 20:39:27 +0430

    setup context API

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,7 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.tsx/mockUser';
-import mockRepos from './mockData.tsx/mockRepos';
-import mockFollowers from './mockData.tsx/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from "react"
+import mockUser from "./mockData.tsx/mockUser"
+import mockRepos from "./mockData.tsx/mockRepos"
+import mockFollowers from "./mockData.tsx/mockFollowers"
+import axios from "axios"
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com"
+const GithubContext = React.createContext()
+type Proptype = {
+  children: React.ReactNode
+}
+const GithubProvider = ({ children }: Proptype) => {
+  return (
+    <GithubContext.Provider value={"haloo"}>{children}</GithubContext.Provider>
+  )
+}
+const useGlobalGithubContext = () => {
+  return useContext(GithubContext)
+}
+export { GithubContext, GithubProvider, useGlobalGithubContext }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Info, Repos, User, Search, Navbar } from "../components"
 import loadingImage from "../images/preloader.gif"
-import { GithubContext } from "../context/context"
+import { useGlobalGithubContext } from "../context/context"
 const Dashboard = () => {
   return (
     <main>
