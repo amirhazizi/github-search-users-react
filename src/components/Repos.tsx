aff487111d341e89215008a0010461c729a7a90d
amir <amirazizihosein@gmail.com>
2023-04-22 11:28:49 +0430

    most Stars/Forks Charts

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -3,16 +3,26 @@ import styled from "styled-components"
 import { useGlobalGithubContext } from "../context/context"
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts"
 type Repos = {
-  repos: { language: string; stargazers_count: number }[]
+  repos: {
+    language: string
+    stargazers_count: number
+    name: string
+    forks: number
+  }[]
 }
 const Repos = () => {
   const { repos } = useGlobalGithubContext() as Repos
 
+  // get Languages
   const languages = repos.reduce((total, item) => {
     const { language, stargazers_count } = item
     if (!language) return total
     if (!total[language]) {
-      total[language] = { label: language, value: 1, stars: stargazers_count }
+      total[language] = {
+        label: language,
+        value: 1,
+        stars: stargazers_count > 0 ? stargazers_count : 1,
+      }
     } else {
       total[language] = {
         ...total[language],
@@ -22,21 +32,45 @@ const Repos = () => {
     }
     return total
   }, {})
-  // console.log("languages", languages)
-
+  // most Used Languages , most Stared Languages
   let mostUsed: { value: number; stars: number }[] = Object.values(languages)
-  let popularLanguages = mostUsed.map((item) => {
+  let mostStared = mostUsed.map((item) => {
     return { ...item, value: item.stars }
   })
 
   mostUsed.sort((a, b) => b.value - a.value).slice(0, 5)
-  popularLanguages.sort((a, b) => b.value - a.value).slice(0, 5)
+  mostStared.sort((a, b) => b.value - a.value).slice(0, 5)
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item
+      total.stars[stargazers_count] = {
+        label: name,
+        value: stargazers_count,
+      }
+      total.forks[forks] = {
+        label: name,
+        value: forks,
+      }
+
+      return total
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  )
+  const mostStars: object[] = Object.values(stars)
+  const mostForks: object[] = Object.values(forks)
+  mostStars.slice(-5).reverse()
+  mostForks.slice(-5).reverse()
   return (
     <section className='section'>
       <Wrapper className='section-center'>
         <Pie3D data={mostUsed} />
-        <div></div>
-        <Doughnut2D data={popularLanguages} />
+        <Column3D data={mostStars} />
+        <Doughnut2D data={mostStared} />
+        <Bar3D data={mostForks} />
         <div></div>
       </Wrapper>
     </section>
